b7dfd7f237b289a6ce20714e33701248fc0d1d9d
Marcin Dłubis <marcindl@hotmail.com>
2022-09-18 12:49:13 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,5 @@
+document.getElementById('btn').addEventListener("click",function (){
+    const val = document.getElementById("inputVal").value;
+    console.log(val)
+    document.getElementById("wypiszDane").innerText = val;
+})
